69284bfb2e788c3cda07e3b9a0499b8ff64a7e71
Hariharan P <hariharanpugazh@gmail.com>
2024-12-14 15:55:59 +0530

    Staff-Dashboard

--- Backend/staff/urls.py
+++ Backend/staff/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from .views import staff_login, staff_signup  # Replace with actual imports if necessary
 from .assessment import create_assessment
 from .Mcq_question import bulk_upload, upload_single_question, fetch_all_questions
+from .views import fetch_contests
+
 urlpatterns = [
     # Authentication
     path("login/", staff_login, name="staff_login"),
     path("signup/", staff_signup, name="staff_signup"),
 
+    # Dashboard
+    path('api/contests/live', fetch_contests, name='fetch_contests'),
     # Assessment API
     path('api/create-assessment/', create_assessment, name='create_assessment'),
 
